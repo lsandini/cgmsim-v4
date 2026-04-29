7f3f62f5eec1a809e083ab9d4e42159cb9ad5a8a
Lorenzo <lsandini@gmail.com>
2026-04-29 23:16:18 +0300

    visual improvements

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git add .
-git commit -m "logistic continuous time compression slider 1-3600x with tooltip"
-git push -u origin dev
+git commit -m "visual improvements"
+git push -u origin visuals
